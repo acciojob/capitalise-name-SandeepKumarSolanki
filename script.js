7483f3832559ee3633d12d994e8076c757936b22
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-02-23 23:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //your JS code here. If required
 let nameBox = document.getElementById('fname');
-input.addEventListener("click",()=>{
-	input.innerText = isCapitalize(nameBox);
-	display(input);
-})
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+
+btn.addEventListener("click", () => {
+    // Get the value from the input field
+    let name = nameBox.value;
+
+    // Convert the name to uppercase
+    let upperName = name.toUpperCase();
+
+    // Display the uppercase name in the output div
+    output.innerText = `${upperName}`;
+});
 
 
